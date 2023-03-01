624f5ef0c4d4a38917d07fecad6f391583e98c34
Bikram <bikramjit421@gmail.com>
2023-02-28 18:45:27 -0800

    updated sell handler

--- TransactionServer/sellHandler.go
+++ TransactionServer/sellHandler.go
@@ -14,12 +14,46 @@ type TriggerOrder struct {
 	Price float64 `json:"price"`
 }
 
+type Sell struct {
+	User   string  `json:"user"`
+	Stock  string  `json:"stock"`
+	Amount float64 `json:"amount"`
+}
+
+
 func sellHandler(w http.ResponseWriter, r *http.Request) {
+
+	var sell Buy
+
+	err := json.NewDecoder(r.Body).Decode(&sell)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Bad Request")
+		return
+	}
+
+	quote, err := strconv.Atoi(GetQuote(sell.Stock, sell.User))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	transaction := db.Transaction{
+		User:   sell.User,
+		Stock:  sell.Stock,
+		Amount: int(sell.Amount),
+		Price:  quote,
+		Is_Buy: false,
+	}
+
+	db.CreateTransaction(transaction)
 
 }
 	
 
 func commitSell(w http.ResponseWriter, r *http.Request) {
+
+	
 	
 }
 
